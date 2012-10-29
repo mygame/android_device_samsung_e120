c67080a12e3cfe78d2532deae6ccc75ff5a3d7e5
Socim <abc@xyz.(none)>
2012-10-29 04:48:38 -0400

    E120S init

---
+++ cm.mk
@@ -0,0 +1,19 @@
+$(call inherit-product, device/samsung/e120s/full_e120s.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120L TARGET_DEVICE=SHV-E120L BUILD_FINGERPRINT="samsung/SHV-E120S/SHV-E120S:4.1.2/IMM76D/JZO54K:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120S-user 4.1.2 IMM76D JZO54K release-keys"
+
+TARGET_BOOTANIMATION_NAME := 720
+#TARGET_BOOTANIMATION_NAME := vertical-720x1280
+
+PRODUCT_NAME := cm_e120s
+PRODUCT_DEVICE := e120s
+
