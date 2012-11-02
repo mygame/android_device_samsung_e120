521291166ff46e54118f7ce27f4249962fdf71b7
Socim <abc@xyz.(none)>
2012-11-02 02:39:44 -0400

    fix mistake

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120L TARGET_DEVICE=SHV-E120L BUILD_FINGERPRINT="samsung/SHV-E120S/SHV-E120S:4.1.2/IMM76D/JZO54K:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120S-user 4.1.2 IMM76D JZO54K release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120S TARGET_DEVICE=SHV-E120S BUILD_FINGERPRINT="samsung/SHV-E120S/SHV-E120S:4.1.2/IMM76D/JZO54K:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120S-user 4.1.2 IMM76D JZO54K release-keys"
 
 TARGET_BOOTANIMATION_NAME := 720
 #TARGET_BOOTANIMATION_NAME := vertical-720x1280
